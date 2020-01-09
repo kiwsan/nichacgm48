7b64fdf2f977e25cb954006185562913b3fa26c5
EKiwsant <kiwsanthia@gmail.com>
2020-01-10 00:42:42 +0700

    added fastland

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -39,7 +39,9 @@ echo storeFile=$APPCENTER_KEYSTORE_FILE >> android/key.properties
 # configs
 cd android
 
-curl -H 'Authorization: token $GITHUB_TOKEN' -H 'Accept: application/vnd.github.v3.raw' --output app/$APPCENTER_KEYSTORE_FILE -L $KEY_STORE_FILE
+KEYSTORE_FILE = app/$APPCENTER_KEYSTORE_FILE
+
+curl -H 'Authorization: token $GITHUB_TOKEN' -H 'Accept: application/vnd.github.v3.raw' --output $KEYSTORE_FILE -L $KEY_STORE_FILE
 
 ls
 ls app
@@ -60,6 +62,6 @@ mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-re
 rm -f $GOOGLE_JSON_FILE
 rm -f android/key.properties
 rm -f android/key.properties
-rm -f android/app/$APPCENTER_KEYSTORE_FILE
+rm -f android/$KEYSTORE_FILE
 
 fi
