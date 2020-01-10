3f772cd56647df5dff235ea27fe9df1e047b8eb0
kiwsant <kiwsanthia@gmail.com>
2020-01-10 11:56:06 +0700

    Create appcenter-pre-build.sh

---
+++ android/app/appcenter-pre-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Example: Change bundle name of an iOS app for non-production
+if [ "$APPCENTER_BRANCH" != "master" ];
+then
+    echo "For example from NuGet, CocoaPods, or Carthage."
+fi
