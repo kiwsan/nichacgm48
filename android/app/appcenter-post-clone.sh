ba7c545fddb7fc6fb1e8394fda4debee921d73e4
kiwsant <kiwsanthia@gmail.com>
2020-01-10 09:24:02 +0700

    Update appcenter-post-clone.sh

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -6,13 +6,18 @@ set -e
 # debug log
 set -x
 
-cd ..
-
-cd app
 
 curl -H 'Authorization: token $GITHUB_TOKEN' -H 'Accept: application/vnd.github.v3.raw' -O -L $KEY_STORE_FILE
 
+
+
+
 cd ..
+
+
+
+
+
 
 # choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
 # stable - recommended for production
