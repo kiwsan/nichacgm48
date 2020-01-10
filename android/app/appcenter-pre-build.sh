88de15a1c0387cc929caf46de644ac5538c9db80
kiwsant <kiwsanthia@gmail.com>
2020-01-10 11:15:18 +0700

    Update appcenter-pre-build.sh

--- android/app/appcenter-pre-build.sh
+++ android/app/appcenter-pre-build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Android Firebase
+GOOGLE_JSON_FILE=android/app/google-services.json
+
+touch $GOOGLE_JSON_FILE
+
+echo "GoogleJson"
+echo "GoogleJson $APPCENTER_SOURCE_DIRECTORY"
+
+if [ -e "$GOOGLE_JSON_FILE" ]
+then
+echo "Updating Google Json"
+echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+echo "File updated"
+
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
