9c86ed00cc4cc2842cdcc99d928a5d1c9aa06258
EKiwsant <kiwsanthia@gmail.com>
2020-01-26 22:54:53 +0700

    implement testing flutter app

---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nichacgm48/ui/home.dart';
+
+void main() {
+  testWidgets('Home page smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    //await tester.pumpWidget(new HomePage());
+
+    // Tap the '+' icon and trigger a frame.
+    //await tester.tap(find.byIcon(Icons.notifications));
+    //await tester.pump();
+  });
+}
