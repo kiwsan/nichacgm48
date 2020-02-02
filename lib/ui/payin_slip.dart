e0a2881ff08606b09bbbff3a1d2ab553ab0371e3
EKiwsant <kiwsanthia@gmail.com>
2020-02-02 11:55:09 +0700

    Upgrading from RxDart 0.22.x to 0.23.x #

---
+++ lib/ui/payin_slip.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PayInSlipPage extends StatefulWidget {
+  @override
+  _PayInSlipPageState createState() => _PayInSlipPageState();
+}
+
+class _PayInSlipPageState extends State<PayInSlipPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
