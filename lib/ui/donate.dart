d651424449bd216908d1f539defd777bca15d3c6
kiwsan <kiwsanthia@gmail.com>
2020-02-23 15:08:34 +0700

    added menu

---
+++ lib/ui/donate.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DonatePage extends StatefulWidget {
+  @override
+  _DonatePageState createState() => _DonatePageState();
+}
+
+class _DonatePageState extends State<DonatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
